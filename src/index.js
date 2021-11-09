946a53060e675dc47b03fa00254264b59b717104
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-09 02:35:37 -0500

    commit tree 4fb81c0507fa91dfd290d1be97f327711978a2d1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
+// target the DOM element we want;form
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // console.log(event);
+    //event.target is in the 
+    // event.target => <form>..</form>
+    // event.target.children[1] => second element
+    // event.target.children[1].value => input value
+    // event.target.children[1].value
+    // get the query #searchById because were trying to make the submit button submit whats in the enter id here
+    const input = document.querySelector('input#searchByID')
+
+    // console.log(input.value);
+                                            // We need to modify the URL we pass to our fetch function based on the input typed into the HTML form
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+        //replace Title and Summary with data we retrieved from our server.
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+        // change the contents of title and summary element based on the data retrieved.
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+
+    //   console.log(data);
+      // LOG: (3) [{…}, {…}, {…}]
+    });  
+  });
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
